0412e6358c314a0e66aa8f8ba07ec71d994608b3
Vadim Kuznetsov <vadimqu@gmail.com>
2020-02-27 13:43:36 +0300

    Add Clip constraint

--- binary_net.py
+++ binary_net.py
@@ -7,3 +7,7 @@ def sign(x, clip=False):
         return tf.where(tf.abs(x) <= 1, dy, tf.zeros_like(x)) if clip else dy
     return tf.where(x < 0, -tf.ones_like(x), tf.ones_like(x)), grad
 # print(sign(0))
+
+class Clip(tf.keras.constraints.Constraint):
+    def __call__(self, w):
+        return tf.clip_by_value(w, -1, 1)
