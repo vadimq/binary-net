9313dcebe0ad1dc6d38797806e330d71b8b1a8fb
Vadim Kuznetsov <vadimqu@gmail.com>
2020-02-28 17:45:27 +0300

    Don't use bias for debugging

--- 7sd.py
+++ 7sd.py
@@ -25,13 +25,13 @@ y = np.array([[1, 1, 1, 1, 1, 1, 0],
               [1, 1, 1, 0, 0, 1, 1]]) * 2 - 1
 
 model = tf.keras.Sequential([
-    binary_net.Dense(100, kernel_constraint=binary_net.Clip()),
+    binary_net.Dense(100, use_bias=False, kernel_constraint=binary_net.Clip()),
     layers.BatchNormalization(momentum=.9, epsilon=1e-4, center=False, scale=False),
     layers.Activation(binary_net.sign_clipped_d),
-    binary_net.Dense(100, kernel_constraint=binary_net.Clip()),
+    binary_net.Dense(100, use_bias=False, kernel_constraint=binary_net.Clip()),
     layers.BatchNormalization(momentum=.9, epsilon=1e-4, center=False, scale=False),
     layers.Activation(binary_net.sign_clipped_d),
-    binary_net.Dense(y.shape[1], kernel_constraint=binary_net.Clip()),
+    binary_net.Dense(y.shape[1], use_bias=False, kernel_constraint=binary_net.Clip()),
     layers.BatchNormalization(momentum=.9, epsilon=1e-4, center=False, scale=False)])
 
 model.compile(optimizer=tf.train.AdamOptimizer(learning_rate=.03),
