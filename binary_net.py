45d437722f134acd99e1e0a7fe755e93a8947236
Vadim Kuznetsov <vadimqu@gmail.com>
2020-02-28 13:46:12 +0300

    Add threshold for debugging

--- binary_net.py
+++ binary_net.py
@@ -13,6 +13,12 @@ def sign_clipped_d(x):
         return tf.where(tf.abs(x) <= 1, dy, tf.zeros_like(x))
     return sign(x), grad
 
+@tf.custom_gradient
+def threshold(input, thresh=.1):
+    def grad(dy):
+        return dy
+    return tf.where(tf.abs(input) < thresh, tf.zeros_like(input), input), grad
+
 class Clip(tf.keras.constraints.Constraint):
     def __call__(self, w):
         return tf.clip_by_value(w, -1, 1)
@@ -20,7 +26,8 @@ class Clip(tf.keras.constraints.Constraint):
 class Dense(tf.keras.layers.Dense):
     def call(self, inputs):
         kernel = self.kernel
-        self.kernel = sign(self.kernel)
+        # self.kernel = sign(self.kernel)
+        self.kernel = sign(threshold(self.kernel))
         rvalue = super(Dense, self).call(inputs)
         self.kernel = kernel
         return rvalue
