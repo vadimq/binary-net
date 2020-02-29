41c93b8b8b4a584874f8d3ea8a6026c19babb0f7
Vadim Kuznetsov <vadimqu@gmail.com>
2020-02-29 11:28:19 +0300

    Set kernel initializers for debugging

--- 7sd.py
+++ 7sd.py
@@ -3,6 +3,12 @@ import tensorflow as tf
 from tensorflow.keras import layers
 import binary_net
 
+# tf.enable_eager_execution()
+
+ki1 = 'glorot_uniform'
+ki2 = 'glorot_uniform'
+ki3 = 'glorot_uniform'
+
 x = np.array([[0, 0, 0, 0],
               [0, 0, 0, 1],
               [0, 0, 1, 0],
@@ -23,15 +29,17 @@ y = np.array([[1, 1, 1, 1, 1, 1, 0],
               [1, 1, 1, 0, 0, 0, 0],
               [1, 1, 1, 1, 1, 1, 1],
               [1, 1, 1, 0, 0, 1, 1]]) * 2 - 1
+# x = tf.convert_to_tensor(x, dtype=tf.float32)
+# y = tf.convert_to_tensor(y, dtype=tf.float32)
 
 model = tf.keras.Sequential([
-    binary_net.Dense(100, use_bias=False, kernel_constraint=binary_net.Clip()),
+    binary_net.Dense(100, use_bias=False, kernel_initializer=ki1, kernel_constraint=binary_net.Clip()),
     layers.BatchNormalization(momentum=.9, epsilon=1e-4, center=False, scale=False),
     layers.Activation(binary_net.sign_clipped_d),
-    binary_net.Dense(100, use_bias=False, kernel_constraint=binary_net.Clip()),
+    binary_net.Dense(100, use_bias=False, kernel_initializer=ki2, kernel_constraint=binary_net.Clip()),
     layers.BatchNormalization(momentum=.9, epsilon=1e-4, center=False, scale=False),
     layers.Activation(binary_net.sign_clipped_d),
-    binary_net.Dense(y.shape[1], use_bias=False, kernel_constraint=binary_net.Clip()),
+    binary_net.Dense(y.shape[1], use_bias=False, kernel_initializer=ki3, kernel_constraint=binary_net.Clip()),
     layers.BatchNormalization(momentum=.9, epsilon=1e-4, center=False, scale=False)])
 
 model.compile(optimizer=tf.train.AdamOptimizer(learning_rate=.03),
