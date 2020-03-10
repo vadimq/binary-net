afdf5496a5efff7c74ca9fda920b854ab2333697
Vadim Kuznetsov <vadimqu@gmail.com>
2020-03-10 15:49:15 +0300

    Load the weights, simplify

--- mnist.py
+++ mnist.py
@@ -7,15 +7,19 @@ import binary_net
 batch_size = 100
 momentum = .9
 # units = 4096
-units = 2048
+# units = 2048
+units = 100
 hidden_layers = 3
-epochs = 1000
+epochs = 5
 dropout_in = 0
 dropout_hidden = 0
 # w_lr_scale = 1
 w_lr_scale = 'Glorot'
 initial_learning_rate = .003
 decay_rate = .0000003 / initial_learning_rate
+
+w = np.load(r"..\BinaryNet\Train-time\weights.npz")
+init = [tf.constant_initializer(w["arr_{}".format(i)]) for i in range(len(w))]
 
 print('Loading data...')
 
@@ -38,11 +42,11 @@ print('Building the model...')
 inputs = tf.keras.Input(shape=(784,))
 x = layers.Dropout(dropout_in)(inputs)
 for i in range(hidden_layers):
-    x = binary_net.Dense(units, w_lr_scale=w_lr_scale, use_bias=False)(x)
+    x = binary_net.Dense(units, w_lr_scale=w_lr_scale, use_bias=False, kernel_initializer=init.pop(0))(x)
     x = layers.BatchNormalization(momentum=momentum, epsilon=1e-4, center=False, scale=False)(x)
     x = layers.Activation(binary_net.sign_d_clipped)(x)
     x = layers.Dropout(dropout_hidden)(x)
-x = binary_net.Dense(10, w_lr_scale=w_lr_scale, use_bias=False)(x)
+x = binary_net.Dense(10, w_lr_scale=w_lr_scale, use_bias=False, kernel_initializer=init.pop(0))(x)
 outputs = layers.BatchNormalization(momentum=momentum, epsilon=1e-4, center=False, scale=False)(x)
 model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
@@ -91,7 +95,7 @@ def train(num_epochs):
     batches = x_train.shape[0] // batch_size
     for i in range(num_epochs):
         start = time.time()
-        x_train, y_train = shuffle(x_train, y_train)
+        # x_train, y_train = shuffle(x_train, y_train)
         loss = 0
         for j in range(batches):
             x_train_slice = x_train[j * batch_size:(j + 1) * batch_size]
@@ -107,6 +111,7 @@ def train(num_epochs):
 
         duration = time.time() - start
         print("Epoch {} of {} took {} s.".format(i + 1, num_epochs, duration))
+        print(model(x_val, training=True)[:2])
         print("  training loss:              {}".format(loss))
         print("  validation loss:            {}".format(result[0]))
         print("  validation error rate:      {}%".format(result[2]))
